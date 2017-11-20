b3cb705c8f13678b1c9b4dd36064517dc75de7e6
msimozar <33079262+msimozar@users.noreply.github.com>
2017-11-20 10:27:47 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -4,47 +4,40 @@ Created on Fri Nov 10 12:54:26 2017
 
 @author: Michael Personal
 """
-$ sudo python
-
-import RPi.GPIO as GPIO
-
 class motor:
     
 	def __init__(self, name, pins):
 		self.name = name
 		self.pins = pins
-		self.speedPin = pins[0]	#define pin that determines speed
-		self.dirPin1 = pins[1]	#define 1st pin that determines direction
-		self.dirPin2 = pins[2]	#define 2nd pin that determines direction
-		
-        
-	def setSpeed(self, speed):
-		self.voltNum = speed
-		self.baseValue = 3.3
-		self.duty = abs(self.voltNum/self.baseValue) #creates duty cycle based on input Voltage value
-		self.sendPower()
-		
+		self.dirPin1 = pins[0]	#define 1st pin that determines direction
+		self.dirPin2 = pins[1]	#define 2nd pin that determines direction
+
 	def pinSetup(self):		
-		GPIO.setup(self.speedPin, GPIO.OUT)    #motor speed pin
 		GPIO.setup(self.dirPin1, GPIO.OUT)    #motor dir1 pin
-		GPIO.setup(self.dirPin2, GPIO.OUT)    #motor dir2 pin
+		GPIO.setup(self.dirPin2, GPIO.OUT)    #motor dir2 pin	
+		my_pwm1 = GPIO.PWM(self.dirPin1, 100) #enables dir1 pin for PWM with frequency 100
+		my_pwm2 = GPIO.PWM(self.dirPin2, 100) #enables dir2 pin for PWM with frequency 100
+		my_pwm1.start(0)	# sets speed value based on duty cycle
+		my_pwm2.start(0)	# sets speed value based on duty cycle
+        
+	def setVolt(self, speed):
+		self.voltNum = speed
+		self.baseValue = 5
+		self.duty = abs(100*self.voltNum/self.baseValue) #creates duty cycle based on input Voltage value
+		self.sendPower()
 	
 	def sendPower():
 		if(self.voltNum != 0):
 			if(self.voltNum > 0):	#set direction
-				GPIO.output(self.dirPin1, True)
-				GPIO.output(self.dirPin2, False)
+				my_pwm1.ChangeDutyCycle(self.duty)	# sets speed value based on duty cycle
+				my_pwm2.ChangeDutyCycle(0)	# sets speed value based on duty cycle
 			else:
-				GPIO.output(self.dirPin1, False)
-				GPIO.output(self.dirPin2, True)
-				
-			my_pwm = GPIO.PWM(self.speedPin, 100)	#creates pwm object
-			my_pwm.start(self.duty)	# sets speed value based on duty cycle
+				my_pwm1.ChangeDutyCycle(0)
+				my_pwm2.ChangeDutyCycle(self.duty)
 		
 		else:	#speed is zero
-			GPIO.output(self.dirPin1, False)
-			GPIO.output(self.dirPin2, False)
-			GPIO.output(self.speedPin, False)
+			my_pwm1.ChangeDutyCycle(0)
+			my_pwm2.ChangeDutyCycle(0)
 
 			
 			
