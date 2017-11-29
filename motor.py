cf1599181700003c0129a4fa9136823637348d83
msimozar <33079262+msimozar@users.noreply.github.com>
2017-11-29 11:05:29 -0600

    Update motor.py
    reconfigured PWM pins

--- motor.py
+++ motor.py
@@ -9,16 +9,17 @@ class motor:
 	def __init__(self, name, pins):
 		self.name = name
 		self.pins = pins
-		self.dirPin1 = pins[0]	#define 1st pin that determines direction
-		self.dirPin2 = pins[1]	#define 2nd pin that determines direction
+		self.speedPin = pins[0]	#define pin that determines speed
+		self.dirPin1 = pins[1]	#define 1st pin that determines direction
+		self.dirPin2 = pins[2]	#define 2nd pin that determines direction
 
 	def pinSetup(self):		
+		GPIO.setup(self.speedPin, GPIO.OUT)    #motor speed pin
 		GPIO.setup(self.dirPin1, GPIO.OUT)    #motor dir1 pin
-		GPIO.setup(self.dirPin2, GPIO.OUT)    #motor dir2 pin	
-		my_pwm1 = GPIO.PWM(self.dirPin1, 100) #enables dir1 pin for PWM with frequency 100
-		my_pwm2 = GPIO.PWM(self.dirPin2, 100) #enables dir2 pin for PWM with frequency 100
-		my_pwm1.start(0)	# sets speed value based on duty cycle
-		my_pwm2.start(0)	# sets speed value based on duty cycle
+		GPIO.setup(self.dirPin2, GPIO.OUT)    #motor dir2 pin
+		self.my_pwm = GPIO.PWM(self.speedPin, 100)	#creates pwm object
+		self.my_pwm.start(0)	# sets speed value based on duty cycle
+
         
 	def setVolt(self, speed):
 		self.voltNum = speed
@@ -29,15 +30,14 @@ class motor:
 	def sendPower():
 		if(self.voltNum != 0):
 			if(self.voltNum > 0):	#set direction
-				my_pwm1.ChangeDutyCycle(self.duty)	# sets speed value based on duty cycle
-				my_pwm2.ChangeDutyCycle(0)	# sets speed value based on duty cycle
+				GPIO.output(self.dirPin1, True)
+				GPIO.output(self.dirPin2, False)
 			else:
-				my_pwm1.ChangeDutyCycle(0)
-				my_pwm2.ChangeDutyCycle(self.duty)
-		
+				GPIO.output(self.dirPin1, False)
+				GPIO.output(self.dirPin2, True)
+				
 		else:	#speed is zero
-			my_pwm1.ChangeDutyCycle(0)
-			my_pwm2.ChangeDutyCycle(0)
-
-			
-			
+			GPIO.output(self.dirPin1, False)
+			GPIO.output(self.dirPin2, False)
+		
+		self.my_pwm.ChangeDutyCycle(self.duty)
