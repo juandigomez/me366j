a1b51ab8c9b0fddd9119c00bc697f492b7af64ac
msimozar <33079262+msimozar@users.noreply.github.com>
2017-12-11 01:12:44 -0600

    Add files via upload

---
+++ straightRun.py
@@ -0,0 +1,41 @@
+def straightRun():
+
+	GPIO.setmode(GPIO.BOARD)
+	
+	motorL = motor("left", [32, 31, 33])
+    encoderA = encoder("left", [26, 29], 50, motorL)
+        
+    motorR = motor("right", [35, 36, 37])
+    encoderB = encoder("left", [38, 40], 50, motorR)
+	
+	proxSensA= psensor("Front",[16,18], 50)
+    proxSensB= psensor("Left",[12,22], 35)
+    proxSensC= psensor("Right",[7,13], 35)
+	
+	rob = robot(motorL, motorR, encoderA, encoderB, proxSensA, proxSensB, proxSensC)
+    rob.pinSetup()
+	
+	rob.direct("forward", 50)
+	while(motorL.voltNum != 0 and motorR.voltNum != 0):
+		rob.straightRun()
+	
+	print("Marsha traveled exactly 10 meters!")
+		
+if __name__== "__main__" :
+        from motor import motor
+        from encoder import encoder
+        from robot import robot
+        from proximity import psensor
+        from TIC import TIC
+        import time
+        import pygame
+        import sys
+        import RPi.GPIO as GPIO
+        from thread import start_new_thread
+        #from multiprocessing import Process
+	try:
+            straightRun()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            GPIO.cleanup()
